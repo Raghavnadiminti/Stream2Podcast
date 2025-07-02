c5700784e6f7d58bdd5c7ae9a6f1cd314189ef1f
Raghavendra <152188358+Raghavnadiminti@users.noreply.github.com>
2025-07-02 16:51:55 +0530

    bug fix for deploy

--- frontend/src/podcast.jsx
+++ frontend/src/podcast.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
-import './Podcast.css';
+import './podcast.css';
 
 function Podcast() {
   const [userId, setUserId] = useState('');
